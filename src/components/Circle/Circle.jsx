333b4083ad33faf1688e13b1580e7de3de4272e3
realmahdi <mahdi.lpr@gmail.com>
2022-05-21 13:25:35 +0430

    Better view of circle component

--- src/components/Circle/Circle.jsx
+++ src/components/Circle/Circle.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 
 function Circle() {
     return (
-        <div>Circle</div>
+        <svg
+            width="100"
+            height="100"
+            viewBox="-50 -50 100 100"
+            className="fill-transparent stroke-[16px] stroke-[#0E2239]"
+        >
+            <circle cx="0" cy="0" r="40"/>
+        </svg>
     );
 }
 
