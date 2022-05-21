16b500d63ff5881c532d5d254a0609257d7c18bc
realmahdi <mahdi.lpr@gmail.com>
2022-05-21 13:28:20 +0430

    Better view of cross component

--- src/components/Cross/Cross.jsx
+++ src/components/Cross/Cross.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 function Cross() {
     return (
-        <div>Cross</div>
+        <svg width="90" height="90" viewBox="-50 -50 100 100"
+             className="stroke-[16px] stroke-[#B9D0E9]">
+            <line x1="-40" y1="-40" x2="40" y2="40"/>
+            <line x1="-40" y1="40" x2="40" y2="-40"/>
+        </svg>
     );
 }
 
