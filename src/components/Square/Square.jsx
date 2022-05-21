161ed609728d330112004a2e076a2ff2dc9a1db1
realmahdi <mahdi.lpr@gmail.com>
2022-05-21 13:06:09 +0430

    Added onClick to Square component

--- src/components/Square/Square.jsx
+++ src/components/Square/Square.jsx
@@ -2,13 +2,24 @@ import React from 'react';
 import Circle from "../Circle/Circle";
 import Cross from "../Cross/Cross";
 
-function Square({pos, value}) {
+function Square({pos, value, onClick}) {
+    const EMPTY = "EMPTY";
     const CIRCLE = "O";
     const CROSS = "X";
+
+    function clickHandler() {
+        if (value === EMPTY) {
+            onClick(pos);
+        }
+    }
+
     return (
-        <div className="w-[100px] h-[100px] flex items-center justify-center bg-white">
+        <div
+            className="w-[100px] h-[100px] flex items-center justify-center bg-white"
+            onClick={clickHandler}
+        >
             {value === CIRCLE && <Circle/>}
-            {value === CROSS && <Cross />}
+            {value === CROSS && <Cross/>}
         </div>
     );
 }
--- src/components/TicTacToeBoard/TicTacToeBoard.jsx
+++ src/components/TicTacToeBoard/TicTacToeBoard.jsx
@@ -15,17 +15,21 @@ function TicTacToeBoard() {
         ]
     });
 
+    function takeTurn(pos) {
+        console.log(pos, 'clicked');
+    }
+
     return (
         <div className="inline-grid grid-cols-3 gap-2 bg-gray-200">
-            <Square pos={0} value={board.positions[0]}/>
-            <Square pos={1} value={board.positions[1]}/>
-            <Square pos={2} value={board.positions[2]}/>
-            <Square pos={3} value={board.positions[3]}/>
-            <Square pos={4} value={board.positions[4]}/>
-            <Square pos={5} value={board.positions[5]}/>
-            <Square pos={6} value={board.positions[6]}/>
-            <Square pos={7} value={board.positions[7]}/>
-            <Square pos={8} value={board.positions[8]}/>
+            <Square pos={0} value={board.positions[0]} onClick={takeTurn}/>
+            <Square pos={1} value={board.positions[1]} onClick={takeTurn}/>
+            <Square pos={2} value={board.positions[2]} onClick={takeTurn}/>
+            <Square pos={3} value={board.positions[3]} onClick={takeTurn}/>
+            <Square pos={4} value={board.positions[4]} onClick={takeTurn}/>
+            <Square pos={5} value={board.positions[5]} onClick={takeTurn}/>
+            <Square pos={6} value={board.positions[6]} onClick={takeTurn}/>
+            <Square pos={7} value={board.positions[7]} onClick={takeTurn}/>
+            <Square pos={8} value={board.positions[8]} onClick={takeTurn}/>
         </div>
     );
 }
