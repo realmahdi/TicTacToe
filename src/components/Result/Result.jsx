d0659dea1db7348953cfa59de8ea96ff2262933e
realmahdi <mahdi.lpr@gmail.com>
2022-05-21 14:59:05 +0430

    Added Result component

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-function Result(props) {
+function Result({winner}) {
+    const CIRCLE = "O";
+    const CROSS = "X";
+    const EQUAL = "EQUAL";
     return (
-        <div>Result</div>
+        <div className="
+          w-[100vw] h-[100vh] absolute
+          flex items-center justify-center
+          flex-col space-y-3
+          text-white
+          bg-gray-400 bg-opacity-90
+          text-9xl text-red-black
+          text-center font-extrabold
+          ">
+            {winner === CIRCLE && "O won the game!"}
+            {winner === CROSS && "X won the game!"}
+            {winner === EQUAL && "XO, Draw!"}
+        </div>
     );
 }
 
