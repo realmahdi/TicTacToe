1ced7bb1b993e7c8f7d5fb4ccb2869df19666384
realmahdi <mahdi.lpr@gmail.com>
2022-05-21 13:14:08 +0430

    Improved rendering of the gameboard

--- src/components/TicTacToeBoard/TicTacToeBoard.jsx
+++ src/components/TicTacToeBoard/TicTacToeBoard.jsx
@@ -15,21 +15,22 @@ function TicTacToeBoard() {
         ]
     });
 
+    const gameBoard = board.positions.map((item, index) => {
+        return <Square
+            pos={index}
+            key={index}
+            value={item}
+            onClick={takeTurn}
+        />
+    })
+
     function takeTurn(pos) {
         console.log(pos, 'clicked');
     }
 
     return (
         <div className="inline-grid grid-cols-3 gap-2 bg-gray-200">
-            <Square pos={0} value={board.positions[0]} onClick={takeTurn}/>
-            <Square pos={1} value={board.positions[1]} onClick={takeTurn}/>
-            <Square pos={2} value={board.positions[2]} onClick={takeTurn}/>
-            <Square pos={3} value={board.positions[3]} onClick={takeTurn}/>
-            <Square pos={4} value={board.positions[4]} onClick={takeTurn}/>
-            <Square pos={5} value={board.positions[5]} onClick={takeTurn}/>
-            <Square pos={6} value={board.positions[6]} onClick={takeTurn}/>
-            <Square pos={7} value={board.positions[7]} onClick={takeTurn}/>
-            <Square pos={8} value={board.positions[8]} onClick={takeTurn}/>
+            {gameBoard}
         </div>
     );
 }
